31d0d9625635de37fc711d7fa1e8173c2f154afb
Alexandra Martinez <nahomibj@gmail.com>
2017-02-01 19:44:41 -0600

    Added beans and implemented setter injection

--- src/main/java/com/martinez/service/CustomerServiceImpl.java
+++ src/main/java/com/martinez/service/CustomerServiceImpl.java
@@ -2,13 +2,16 @@ package com.martinez.service;
 
 import com.martinez.model.Customer;
 import com.martinez.repository.CustomerRepository;
-import com.martinez.repository.HibernateCustomerRepositoryImpl;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+    private CustomerRepository customerRepository;
+
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     @Override
     public List<Customer> findAll() {
